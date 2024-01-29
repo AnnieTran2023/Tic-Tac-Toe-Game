af5f072343911755468f60a7581ba4c87bacfc8b
Annie Tran <tranthilananh105@gmail.com>
2024-01-30 01:51:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+import { TicTacToe } from './Components/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div className="App">
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
